8cf8fa7bae0dffb321a1a7bbd70783ac93e217eb
Jay Wineinger <jay.wineinger@gmail.com>
2014-12-04 22:36:13 -0600

    Add logging; prints INFO to stdout by default, verbose mode changes to DEBUG

--- copy_on_host/copy_on_host.py
+++ copy_on_host/copy_on_host.py
@@ -4,17 +4,53 @@ Python CLI script to copy a file on a Docker host from within a container.
 from __future__ import print_function
 import argparse
 import json
+import logging
+import logging.config
 import os.path
 
 from docker.client import Client
 from docker.utils import kwargs_from_env
 
 
+LOG = logging.getLogger(__name__)
+
+
+def configure_logging():
+    logging.config.dictConfig({
+        'version': 1,
+        'disable_existing_loggers': True,
+        'formatters': {
+            'standard': {
+                'format': '%(asctime)s [%(levelname)s]: %(message)s'
+            },
+        },
+        'handlers': {
+            'default': {
+                'level': 'DEBUG',
+                'class': 'logging.StreamHandler',
+                'formatter': 'standard',
+            },
+        },
+        'loggers': {
+            '': {
+                'handlers': ['default'],
+                'level': 'WARNING',
+                'propagate': False,
+            },
+            '__main__': {
+                'handlers': ['default'],
+                'level': 'INFO',
+                'propagate': False,
+            },
+        }
+    })    
+
+
 def copy_file_via_docker(client, image_name, src_path, dest_path):
     """
     Copies a file from one location on the Docker host to another location on the Docker host, but from
     within a Docker container. This function will block/wait until the command is finished and then return
-    the exit code and any output to stdout and stderr.
+    the exit code and any output to stdout and stderr. Returns a 3-tuple of (exit code, stdout, stderr).
 
     :param client: docker.client.Client instance
     :param image_name: string name of the docker image to use
@@ -22,17 +58,24 @@ def copy_file_via_docker(client, image_name, src_path, dest_path):
     :param dest_path: string, a full path to the destination on the docker host to copy `src_path` to
     :return: 3-tuple, (command exit code, command stdout, command stderr)
     """
-    # Use the Docker `client` to pull a given image name. The output of this command is printed in
-    # a streaming fashion in order to give the user feedback as to what is going on. Downloading an image can
-    # take a long time depending on the size of the image and the user's network bandwidth.
+    # Use the Docker client to pull a given image name. Downloading images can take a long time depending on the
+    # size of the image and the user's network bandwidth. Download progress can be seen in verbose mode
+    LOG.info("pulling docker image '{image}'".format(image=image_name))
     for line in client.pull(image_name, stream=True):
-        print(json.dumps(json.loads(line), indent=4))
+        data = json.loads(line)
+        if data['status'].lower() == 'downloading':
+            LOG.debug("{id}: {progress}".format(**data))
+        elif 'id' in data:
+            LOG.debug("{id}: {status}".format(**data))
+        else:
+            LOG.debug("{status}".format(**data))
 
     # Choose what path to mount from the host filesystem and where to mount it within the container. Mounting / is not
     # the best idea but for the sake of this demo script, we're going to do it so that any path given can be copied if
     # it is valid on the host
     host_path = '/'
     mount_point = '/mnt/host'
+    LOG.info("host filesystem '{host}' will be mounted at '{mount}'".format(host=host_path, mount=mount_point))
 
     # Setup the paths that will be given as arguments to the copy command by prepending the chosen mount point to
     # the `src_path` and `dest_path` arguments to make them valid within the container (assuming that the original
@@ -40,25 +83,35 @@ def copy_file_via_docker(client, image_name, src_path, dest_path):
     # Strip off any leading slash on `src_path` and `dest_path` because os.path.join() will lose the mount_point prefix
     # if the second part starts with a slash.
     src = os.path.join(mount_point, src_path.lstrip("/"))
+    LOG.debug("src path: '{path}'".format(path=src))
     dest = os.path.join(mount_point, dest_path.lstrip("/"))
+    LOG.debug("dest path: '{path}'".format(path=dest))
 
     # set up the `cp` command using the paths under the mount-point
     cmd = ["cp", src, dest]
+    LOG.debug("command = '{cmd}'".format(cmd=" ".join(cmd)))
 
+    LOG.debug("creating container")
     container = client.create_container(image=image_name, command=cmd, volumes=[mount_point])
 
-    # start the container and bind the host's root to the mount point
+    # start the container and bind the host's filesystem to the mount point
+    LOG.info("starting container")
     client.start(container=container, binds={host_path: {'bind': mount_point, 'ro': False}})
 
     # wait until the command is complete and get its exit code
+    LOG.debug("waiting until command is complete")
     exit_code = client.wait(container)
+    LOG.log(msg="exit code was {exit}".format(exit=exit_code),
+            level=logging.WARNING if exit_code > 0 else logging.DEBUG)
 
     # get the stdout and stderr of the command. it does seem that we can get stdout and stderr separately,
     # but that has the downside that we would lose the output order. The next line could be used instead to
     # get stdout and stderr interleaved together and maintain output order.
     # output = client.logs(container, stdout=True, stderr=True)
-    stderr = client.logs(container, stdout=False, stderr=True).strip()
+    LOG.debug("getting stdout")
     stdout = client.logs(container, stdout=True, stderr=False).strip()
+    LOG.debug("getting stderr")
+    stderr = client.logs(container, stdout=False, stderr=True).strip()
 
     return exit_code, stdout, stderr
 
@@ -85,14 +138,18 @@ def main():
     :return: None
     """
     def full_path(path):
-        """Converts path arguments to full absolute paths, expanding user shortcuts if given"""
+        """Converts path arguments to full absolute paths, expanding user shortcuts if given."""
         return os.path.abspath(os.path.expanduser(path))
 
     parser = argparse.ArgumentParser(description='Copy a file from within a docker container')
     parser.add_argument('src_path', help='The path of the file on the host to copy.', type=full_path)
     parser.add_argument('dest_path', help='The path on the host to copy the file to.', type=full_path)
     parser.add_argument('-i', '--image', default='ubuntu:14.04', help='Docker image to use')
+    parser.add_argument('-v', '--verbose', action='store_true', help='Increase logging level (from INFO to DEBUG)')
     args = parser.parse_args()
+
+    if args.verbose:
+        LOG.setLevel(logging.DEBUG)
 
     exit_code, stdout, stderr = copy_file_via_docker(client=setup_client(), image_name=args.image,
                                                      src_path=args.src_path, dest_path=args.dest_path)
@@ -103,7 +160,9 @@ def main():
         print("Stdout:\n{out}".format(out=stdout))
     if stderr:
         print("Stderr:\n{err}".format(err=stderr))
-
+    if not stdout and not stderr:
+        print("No output")
 
 if __name__ == '__main__':
+    configure_logging()
     main()
